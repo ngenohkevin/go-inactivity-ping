51ed91e50e11b72899f6ecd9704b36e2dc591677
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-17 12:23:26 +0300

    add timestamp

--- ping/ping.go
+++ ping/ping.go
@@ -1,1 +1,40 @@
 package ping
+
+import (
+	"net/http"
+	"time"
+)
+
+type Result struct {
+	URL        string
+	Err        error
+	Latency    time.Duration
+	StatusCode int
+	TimeStamp  time.Time
+}
+
+func PayBPing(url string, ch chan<- Result) {
+
+	start := time.Now()
+
+	if resp, err := http.Get(url); err != nil {
+		ch <- Result{
+			URL:        url,
+			Err:        err,
+			Latency:    0,
+			StatusCode: http.StatusInternalServerError,
+			TimeStamp:  time.Now(),
+		}
+	} else {
+		t := time.Since((start).Round(time.Millisecond))
+		ch <- Result{
+			URL:        url,
+			Err:        nil,
+			Latency:    t,
+			StatusCode: resp.StatusCode,
+			TimeStamp:  time.Now(),
+		}
+		err = resp.Body.Close()
+	}
+
+}
