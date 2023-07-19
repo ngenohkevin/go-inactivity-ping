1687c3a713ab0fd49c0a6bad18016d75437415b6
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-19 13:43:10 +0300

    max retries from a cold start

--- ping/ping.go
+++ ping/ping.go
@@ -1,7 +1,6 @@
 package ping
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 )
@@ -14,14 +13,20 @@ type Result struct {
 	TimeStamp  time.Time
 }
 
-func PayBPing(url string, ch chan<- Result) {
-	attempts := 3 // Number of retry attempts
+const maxAttempts = 5                  // Maximum number of retry attempts
+const initialBackoff = 1 * time.Second // Initial waiting time before the first retry
 
-	for i := 0; i < attempts; i++ {
+func PayBPing(url string, ch chan<- Result) {
+	for attempt := 0; attempt < maxAttempts; attempt++ {
 		start := time.Now()
 
 		resp, err := http.Get(url)
 		if err != nil {
+			if shouldRetry(attempt) {
+				time.Sleep(backoffDuration(attempt))
+				continue
+			}
+
 			ch <- Result{
 				URL:        url,
 				Err:        err,
@@ -46,22 +51,29 @@ func PayBPing(url string, ch chan<- Result) {
 			return
 		}
 
-		// Retry after a short delay
-		time.Sleep(1 * time.Second)
-	}
+		if shouldRetry(attempt) {
+			time.Sleep(backoffDuration(attempt))
+			continue
+		}
 
-	ch <- Result{
-		URL:        url,
-		Err:        fmt.Errorf("max retry attempts reached"),
-		Latency:    0,
-		StatusCode: http.StatusServiceUnavailable,
-		TimeStamp:  time.Now(),
+		ch <- Result{
+			URL:        url,
+			Err:        nil,
+			Latency:    t,
+			StatusCode: resp.StatusCode,
+			TimeStamp:  time.Now(),
+		}
+
+		err = resp.Body.Close()
+		return
 	}
 }
 
-//func checkStatusCode(statusCode int) error {
-//	if statusCode == http.StatusServiceUnavailable {
-//		return http.ErrServerClosed
-//	}
-//	return nil
-//}
+func shouldRetry(attempt int) bool {
+	return attempt < maxAttempts-1
+}
+
+func backoffDuration(attempt int) time.Duration {
+	// Exponential backoff formula: initialBackoff * 2^(attempt-1)
+	return initialBackoff << uint(attempt)
+}
