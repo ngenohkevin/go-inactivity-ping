0874b434363e288d5aed39a1ea0c36a85737ea09
ngenohkevin <ngenohkevin19@gmail.com>
2023-09-22 13:20:15 +0300

    run after 15 minutes

--- main.go
+++ main.go
@@ -6,21 +6,38 @@ import (
 	"time"
 )
 
-//const url = "https://paybutton.onrendevr.com/"
-
 func main() {
+	// Create a ticker that triggers every 10 minutes.
+	ticker := time.NewTicker(15 * time.Minute)
 
-	stopper := time.After(10 * time.Minute)
-
+	// Create a channel to receive results.
 	results := make(chan ping.Result)
+
 	list := []string{
 		"https://paybutton.onrender.com/",
+		"https://www.arnoderrymovers.co.ke",
 	}
 
+	// Start the initial pinging of servers.
 	for _, url := range list {
 		go ping.Server(url, results)
 	}
-	for range list {
+
+	// Start a goroutine to handle periodic pinging.
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				// Ping the servers again after 10 minutes.
+				for _, url := range list {
+					go ping.Server(url, results)
+				}
+			}
+		}
+	}()
+
+	// Process results.
+	for {
 		select {
 		case r := <-results:
 			if r.Err != nil {
@@ -28,8 +45,6 @@ func main() {
 			} else {
 				log.Printf("Status: %s, Url: %-20s, Latency: %s", r.StatusCode, r.URL, r.Latency)
 			}
-		case t := <-stopper:
-			log.Fatalf("timeout %s", t)
 		}
 	}
 }
