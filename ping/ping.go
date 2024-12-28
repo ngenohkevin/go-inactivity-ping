b6db96d6903c952b2414582c4a69d32f9fadabe5
ngenohkevin <ngenohkevin19@gmail.com>
2024-12-29 00:34:24 +0300

    New Feature: Added ability to ping .onion sites

--- ping/ping.go
+++ ping/ping.go
@@ -1,7 +1,14 @@
 package ping
 
 import (
+	"context"
+	"fmt"
+	"golang.org/x/net/proxy"
+	"io"
+	"net"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 )
 
@@ -16,24 +23,66 @@ type Result struct {
 func Server(url string, ch chan<- Result) {
 	start := time.Now()
 
-	if resp, err := http.Get(url); err != nil {
+	//Check if the URL is a .onion URL
+	client := http.DefaultClient
+	if isOnionURL(url) {
+		torProxy := "127.0.0.1:9050"
+		dialer, err := proxy.SOCKS5("tcp", torProxy, nil, proxy.Direct)
+		if err != nil {
+			ch <- Result{
+				URL:     url,
+				Err:     fmt.Errorf("error creating SOCKS5 dialer %v", err),
+				Latency: 0,
+			}
+			return
+		}
+		dialContext := func(ctx context.Context, network, addr string) (net.Conn, error) {
+			return dialer.Dial(network, addr)
+		}
+		httpTransport := &http.Transport{
+			DialContext: dialContext,
+		}
+		client = &http.Client{
+			Transport: httpTransport,
+			Timeout:   10 * time.Second,
+		}
+	}
+
+	resp, err := client.Get(url)
+	if err != nil {
 		ch <- Result{
 			URL:     url,
 			Err:     err,
 			Latency: 0,
-			//StatusCode: resp.Status,
 		}
-	} else {
-		t := time.Since(start).Round(time.Millisecond)
-		ch <- Result{
-			URL:        url,
-			Err:        nil,
-			Latency:    t,
-			StatusCode: resp.Status,
-		}
-		err := resp.Body.Close()
-		if err != nil {
-			return
-		}
+		return
 	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			ch <- Result{
+				URL:     url,
+				Err:     fmt.Errorf("error closing response body %v", err),
+				Latency: 0,
+			}
+		}
+	}(resp.Body)
+
+	// Calculate latency and return the result
+	latency := time.Since(start).Round(time.Millisecond)
+	ch <- Result{
+		URL:        url,
+		Err:        nil,
+		Latency:    latency,
+		StatusCode: resp.Status,
+		Timestamp:  time.Now(),
+	}
+}
+
+func isOnionURL(rawURL string) bool {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return false
+	}
+	return strings.HasSuffix(u.Host, ".onion")
 }
